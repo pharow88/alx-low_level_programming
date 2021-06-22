bc8dbe3663dca20b676f660c494798a477b74333
Pharow Mwale <pharows88@gmail.com>
2021-06-22 15:26:36 -0700

    Loops

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
-
 /**
  * struct formate - structure to choose the format
  * @ftype: type of format to print
@@ -23,6 +22,19 @@ typedef struct formate
 
 } f_type;
 
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
 int (*get_format(char s))(va_list);
 int _printf(const char *format, ...);
 int print_char(va_list arg);
@@ -36,4 +48,5 @@ int _islower(int c);
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
+void test_islower(int n);
 #endif
---
+++ 0x02-functions_nested_loops/main_0.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * test_islower - Test the _islower function
+ *
+ * @n: Number to pass to _islower function
+ */
+
+void test_islower(int n)
+
+{	
+	int r;
+
+	r = _islower(n);
+	putchar(r + '0');
+	putchar('\n');
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	test_islower('H');
+	return (0);
+}
